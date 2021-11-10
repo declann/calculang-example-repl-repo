876eea3ccd25fe690f5c148068738e6a845d7ca9
Declan Naughton <declan.naughton2@mail.dcu.ie>
2021-11-10 18:52:03 +0000

    ->97 cost

--- src/base.cul.js
+++ src/base.cul.js
@@ -3,7 +3,7 @@ export const revenue = () => {
 };
 
 // variable costs only, OK for testing
-export const costs = () => 98 * units();
+export const costs = () => 97 * units();
 
 export const profit = () => revenue() - costs();
 
